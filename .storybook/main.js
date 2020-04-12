a2519822f97901fd4b7c03a2d1c32b4ed47470ec
reallifeliver <pms3300@gmail.com>
2020-04-12 13:20:32 +0900

    addon-docs 적용

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register',
+    //'@storybook/addon-docs',
+    {
+      name : '@storybook/addon-docs',
+      options : {
+        configureJSX : true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      }
+    }
   ],
 };
--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+/**
+ * 안녕하세요라고 보여주고 싶을때는 Hello컴포넌트를 사용하세요.
+ * 
+ * - big을 true로 설정하면 컴포넌트가 **크게** 나타납니다.
+ * 함수도 지정할 수 있어요.
+ *  */
 const Hello = ({ name, big, onHello, onBye }) => {
   return (
     <div>
@@ -11,5 +18,20 @@ const Hello = ({ name, big, onHello, onBye }) => {
     </div>
   );
 };
+
+Hello.propTypes= {
+    /** 보여주고 싶은 이름 */
+    name: PropTypes.string.isRequired,
+    /**true 설정시 h1태그로 렌더링 */
+    big : PropTypes.bool,
+    /** Hello버튼 누를때 호출 */
+    onHello: PropTypes.func,
+    /** Bye버튼을 누를때 호출 */
+    onBye : PropTypes.func
+}
+
+Hello.defaultProps = {
+    big: false
+}
 
 export default Hello;
